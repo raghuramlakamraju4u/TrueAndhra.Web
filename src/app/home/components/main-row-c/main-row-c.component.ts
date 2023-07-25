729d87b4176a8b158cf41955a4426bb845d6c305
sathish <sathmech@gmail.com>
2023-07-25 22:17:08 +0530

    row-c is filled

--- src/app/home/components/main-row-c/main-row-c.component.ts
+++ src/app/home/components/main-row-c/main-row-c.component.ts
@@ -7,6 +7,120 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainRowCComponent implements OnInit {
 
+
+  public oFirstColumn={
+    sTitle: 'LifeStyle',
+    arrPosts: [
+      {
+        sImg: 'https://quanticalabs.com/wp_themes/pressroom/files/2015/01/image_031-510x374.jpg',
+        STitle: 'North Sea oil industry faces dire future',
+        sDesc: 'Maecenas mauris elementum, est morbi interdum cursus at elite imperdiet libero. Proin odios dapibus integer an nulla augue pharetra cursus.',
+        sCat: 'LIFESTYLE',
+        oDate: new Date(),
+        nImgSize: 3,
+        bAlignHorizontal: false,
+        bShowBullets: false,
+        arrBullets: [
+          'Free episodes and new movies on iTunes',
+          'Getting started with mobile games',
+          'The world’s tiniest drone put to the test',
+          'Our favorites from the Geneva Motor Show'
+        ]
+      },
+      {
+        sImg: 'https://quanticalabs.com/wp_themes/pressroom/files/2015/01/image_06-100x100.jpg',
+        STitle: 'North Sea oil industry faces dire future',
+        sDesc: 'Maecenas mauris elementum, est morbi interdum cursus at elite imperdiet libero. Proin odios dapibus integer an nulla augue pharetra cursus.',
+        sCat: 'LIFESTYLE',
+        oDate: new Date(),
+        nImgSize: 0,
+        bAlignHorizontal: true,
+        bShowBullets: false,
+        arrBullets: [
+          'Free episodes and new movies on iTunes',
+          'Getting started with mobile games',
+          'The world’s tiniest drone put to the test',
+          'Our favorites from the Geneva Motor Show'
+        ]
+      }
+    ]
+  }
+
+  public oSecondColumn={
+    sTitle: 'Health',
+    arrPosts: [
+      {
+        sImg: 'https://quanticalabs.com/wp_themes/pressroom/files/2015/01/image_11-330x242.jpg',
+        STitle: 'North Sea oil industry faces dire future',
+        sDesc: 'Maecenas mauris elementum, est morbi interdum cursus at elite imperdiet libero. Proin odios dapibus integer an nulla augue pharetra cursus.',
+        sCat: 'LIFESTYLE',
+        oDate: new Date(),
+        nImgSize: 3,
+        bAlignHorizontal: false,
+        bShowBullets: false,
+        arrBullets: [
+          'Free episodes and new movies on iTunes',
+          'Getting started with mobile games',
+          'The world’s tiniest drone put to the test',
+          'Our favorites from the Geneva Motor Show'
+        ]
+      },
+      {
+        sImg: 'https://quanticalabs.com/wp_themes/pressroom/files/2015/01/image_13-100x100.jpg',
+        STitle: 'North Sea oil industry faces dire future',
+        sDesc: 'Maecenas mauris elementum, est morbi interdum cursus at elite imperdiet libero. Proin odios dapibus integer an nulla augue pharetra cursus.',
+        sCat: 'LIFESTYLE',
+        oDate: new Date(),
+        nImgSize: 0,
+        bAlignHorizontal: true,
+        bShowBullets: false,
+        arrBullets: [
+          'Free episodes and new movies on iTunes',
+          'Getting started with mobile games',
+          'The world’s tiniest drone put to the test',
+          'Our favorites from the Geneva Motor Show'
+        ]
+      }
+    ]
+  }
+
+  public oThirdColumn={
+    sTitle: 'Science',
+    arrPosts: [
+      {
+        sImg: 'https://quanticalabs.com/wp_themes/pressroom/files/2015/01/image_04-510x374.jpg',
+        STitle: 'North Sea oil industry faces dire future',
+        sDesc: 'Maecenas mauris elementum, est morbi interdum cursus at elite imperdiet libero. Proin odios dapibus integer an nulla augue pharetra cursus.',
+        sCat: 'LIFESTYLE',
+        oDate: new Date(),
+        nImgSize: 3,
+        bAlignHorizontal: false,
+        bShowBullets: false,
+        arrBullets: [
+          'Free episodes and new movies on iTunes',
+          'Getting started with mobile games',
+          'The world’s tiniest drone put to the test',
+          'Our favorites from the Geneva Motor Show'
+        ]
+      },
+      {
+        sImg: 'https://quanticalabs.com/wp_themes/pressroom/files/2015/01/image_02-100x100.jpg',
+        STitle: 'North Sea oil industry faces dire future',
+        sDesc: 'Maecenas mauris elementum, est morbi interdum cursus at elite imperdiet libero. Proin odios dapibus integer an nulla augue pharetra cursus.',
+        sCat: 'LIFESTYLE',
+        oDate: new Date(),
+        nImgSize: 0,
+        bAlignHorizontal: true,
+        bShowBullets: false,
+        arrBullets: [
+          'Free episodes and new movies on iTunes',
+          'Getting started with mobile games',
+          'The world’s tiniest drone put to the test',
+          'Our favorites from the Geneva Motor Show'
+        ]
+      }
+    ]
+  }
   constructor() { }
 
   ngOnInit(): void {
